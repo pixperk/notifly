82ec79ea496e28519d621146e23511f5c3b83961
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-06-07 11:48:04 +0530

    feat(notification) : entry point

--- notification/cmd/notification/main.go
+++ notification/cmd/notification/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/pixperk/notifly/common"
+	"github.com/pixperk/notifly/notification"
+	"github.com/pixperk/notifly/notification/util"
+)
+
+func main() {
+
+	cfg, err := notification.LoadConfig(".")
+	if err != nil {
+		log.Fatalf("Cannot load config: %v", err)
+	}
+
+	nc, err := util.ConnectNats(cfg.NatsURL, cfg.NatsClientID)
+	if err != nil {
+		log.Fatalf("Cannot connect to NATS: %v", err)
+	}
+
+	queue := make(chan common.NotificationEvent, 100)
+
+	err = util.SubscribeToNotifications(nc, queue)
+	if err != nil {
+		log.Fatalf("NATS subscription failed: %v", err)
+	}
+
+	log.Printf("Started...")
+	util.StartWorkerPool(queue, 5)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+}
---
+++ notification/config.go
@@ -0,0 +1,30 @@
+package notification
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	NatsURL       string `mapstructure:"NATS_URL"`
+	NatsClusterID string `mapstructure:"NATS_CLUSTER_ID"`
+	NatsClientID  string `mapstructure:"NATS_CLIENT_ID"`
+	Port          int    `mapstructure:"PORT"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	if err = viper.Unmarshal(&config); err != nil {
+		return
+	}
+
+	return
+}
