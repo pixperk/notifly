78c82b68c88c1bc0058ca95cb1b8c40a44ebdcfd
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-06-08 11:39:22 +0530

    refactor(notification) : separation of concerns in consumer

--- notification/util/nats.go
+++ notification/util/nats.go
@@ -1,3 +1,4 @@
+// util/nats.go
 package util
 
 import (
@@ -15,59 +16,59 @@ import (
 func ConnectNats(natsUrl, clientId string) (*nats.Conn, error) {
 	opts := []nats.Option{
 		nats.Name(clientId),
-		nats.MaxReconnects(-1),       // Infinite reconnects
-		nats.ReconnectWait(2 * 1000), // 2 seconds wait between reconnects
+		nats.MaxReconnects(-1),
+		nats.ReconnectWait(2 * time.Second),
 	}
 
 	nc, err := nats.Connect(natsUrl, opts...)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to NATS: %w", err)
 	}
-
 	return nc, nil
 }
 
 func SubscribeToNotifications(nc *nats.Conn, cfg notification.Config) error {
 	subj := "notifications.*"
-	var maxRetries int
 	js, err := nc.JetStream()
 	if err != nil {
 		return fmt.Errorf("failed to get JetStream context: %w", err)
 	}
 
 	_, err = js.QueueSubscribe(subj, "notif-workers",
-		func(msg *nats.Msg) {
-			var event common.NotificationEvent
-			if err := json.Unmarshal(msg.Data, &event); err != nil {
-				log.Printf("Error unmarshalling message: %v", err)
-				msg.Ack() //Ack to discard bad message
-				return
-			}
-
-			maxRetries = getMaxRetriesByType(event)
-
-			err := processJob(event, cfg)
-			if err != nil {
-				if err == util.ErrInvalidPhoneNumber {
-					log.Printf("Invalid phone number for job %v: %v", event.NotificationId, err)
-					msg.Ack() // Ack to discard bad message
-					return
-				}
-				log.Printf("Error processing job %v: %v", event.NotificationId, err)
-				msg.Nak() // Nack to retry the message
-				return
-			} else {
-				log.Printf("Successfully processed job %v", event.NotificationId)
-				msg.Ack() // Acknowledge successful processing
-			}
-
-		},
+		buildHandler(cfg),
 		nats.Durable("notif-consumer"),
 		nats.AckExplicit(),
-		nats.MaxDeliver(maxRetries),
+		nats.ManualAck(),
+		nats.MaxDeliver(5),
 		nats.AckWait(30*time.Second),
-		nats.ManualAck())
+	)
 
 	return err
+}
 
+func buildHandler(cfg notification.Config) nats.MsgHandler {
+	return func(msg *nats.Msg) {
+		var event common.NotificationEvent
+		if err := json.Unmarshal(msg.Data, &event); err != nil {
+			log.Printf("JSON unmarshal failed: %v", err)
+			_ = msg.Term()
+			return
+		}
+
+		err := processJob(event, cfg)
+		if err != nil {
+			if err == util.ErrInvalidPhoneNumber {
+				log.Printf("Invalid phone number: %v", event.NotificationId)
+				_ = msg.Term()
+				return
+			}
+
+			log.Printf("Retry job %v due to error: %v", event.NotificationId, err)
+			_ = msg.Nak()
+			return
+		}
+
+		log.Printf("Job processed: %v", event.NotificationId)
+		_ = msg.Ack()
+	}
 }
